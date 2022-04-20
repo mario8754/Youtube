5691b801dc9ca7229ce5ef8aac4900afb9738cb2
Mario <mario.dulder@hotmail.com>
2022-04-20 19:42:06 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import webbrowser
+import time
+
+
+url = "https://www.youtube.com/watch?v=SFH6fzRly7g"
+
+views = 10
+
+duration = 20
+
+for i in range(int(views)):
+
+    webbrowser.open_new(url)
+
+    time.sleep(int(duration))
